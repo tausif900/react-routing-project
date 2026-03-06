7232d8348e836f77a4fae0a2d18acec0d5bf60e0
tausif900 <ansaritausif9268@gmail.com>
2026-03-06 08:05:51 +0530

    navigate

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
+
+// Conditional rendering
 function Register() {
+  const [name, setName] = useState('')
+  navigate = useNavigate()
+  const redirect = () => {
+    if (name) {
+      navigate('/');
+    } else {
+      navigate('contact')
+    }
+  }
   return (
     <div>
       Register
+      <button onClick={redirect}>Click</button>
     </div>
   )
 }
